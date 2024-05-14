f2d49ffba3b1a4077c2126855719407a0a69cb17
Mehedi7242 <mehedi2hasan7@gmail.com>
2024-05-14 20:14:11 +0600

    first changes

---
+++ index.js
@@ -0,0 +1,26 @@
+//calculate program
+
+const display =document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+
+function clearToDisplay(){
+    display.value= "";
+}
+
+
+function calculate(){
+    try{
+
+    
+    display.value=eval(display.value)
+    }
+    catch(error){
+        display.value="error";
+
+    }
+
+}
